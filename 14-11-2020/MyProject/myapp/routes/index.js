3ef15bfb76b5bb7813c07032e245b6c6322d941c
piyush281097 <piyushpatel281097@gmail.com>
2020-12-18 04:01:48 -0500

    Create index.js

---
+++ 14-11-2020/MyProject/myapp/routes/index.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();  
+var DB = require('../database');
+var crypto = require('crypto');
+session = require('express-session');
+const { route } = require('.');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('login', { title: 'Express' ,error : req.query.error == 1 ? 1 : 0 ,success : req.query.success == 1 ? 1 : 0 });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Express' ,email: req.query.email == 1 ? 1 : 0 });
+});
+
+router.post('/login', function(req, res, next) {
+  
+  var hash = crypto.createHash('sha1');
+  shapass = hash.update('req.body.pass', 'utf-8').digest('hex')
+  console.log(req.body.email,shapass);
+  var projectsSql = 'select * from user where email_id = ? and password = ?';
+  DB.query(projectsSql, [req.body.email,shapass], function(err, users) { 
+      if (err) {
+      res.status(500).send(JSON.stringify(err));
+      return;
+      }
+
+      if (users.length > 0) {
+        req.session.user = users[0];
+        res.redirect('/dashboard')
+      }else {
+        res.redirect('/?error=1')
+      }   
+    });
+})
+
+router.get('/dashboard', function(req, res, next) {
+  if (!user) {
+    res.redirect('/')
+  }
+  console.log(user);
+
+    res.render('dashboard',{'user1' : user});
+});
+
+router.post('/create_account',function (req, res) {
+  
+  var hash = crypto.createHash('sha1');
+  shapass = hash.update('req.body.pass', 'utf-8').digest('hex')
+
+  var selectSql = 'select * from user where email_id = ? ';
+  DB.query(selectSql, [req.body.email], function(err, selectUser) { 
+      if (err) {
+      res.status(500).send(JSON.stringify(err));
+      return;
+      }
+      if (selectUser.length > 0) {
+        res.redirect('/signup'+'?email=1')
+      }else {
+        var insertSql = 'insert into user (firstname,lastname,email_id,password) value (?,?,?,?)';
+        DB.query(insertSql, [req.body.firstname,req.body.lastname,req.body.email,shapass], function(err, insert_record) { 
+            if (err) {
+            res.status(500).send(JSON.stringify(err));
+            return;
+            }
+            console.log(insert_record);
+        res.redirect('/?success=1')
+      })
+    }
+  })
+})
+
+router.get('/logout', function(req, res, next) {
+  if (!user) {
+    res.redirect('/')
+  }
+  console.log(user);
+});
+
+
+module.exports = router;
