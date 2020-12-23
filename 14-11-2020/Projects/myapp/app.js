1798afa16503366a82815ba95ebe6b478f1d5e37
piyush281097 <piyushpatel281097@gmail.com>
2020-12-23 06:43:32 -0500

    Update app.js

--- 14-11-2020/Projects/myapp/app.js
+++ 14-11-2020/Projects/myapp/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var projectRouter = require('./routes/project');
+var projectRouter = require('./routes/projects');
 
 var app = express();
 
