249e80d187ef36b9c01cbe3c993917ee14ac15fa
piyush281097 <piyushpatel281097@gmail.com>
2021-01-16 04:40:08 -0500

    Create interfaces.js

---
+++ 07-01-2021/interfaces.js
@@ -0,0 +1,19 @@
+console.log("Interfaces Example >>> ");
+function printLabel(labeledObj) {
+    console.log(labeledObj.label);
+}
+var myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
+console.log("Interface Example 2 >> ");
+function createSquare(config) {
+    var newSquare = { color: "white", area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySquare = createSquare({ color: "black" });
+console.log(mySquare);
