e2c78a6a6c3a7dac3365d7f5daf43f736ca77c20
piyush281097 <piyushpatel281097@gmail.com>
2021-03-04 08:37:52 -0500

    Create Dashboard.js

---
+++ todo/frontend/src/Component/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Welcome to Dashboard</h2>
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
