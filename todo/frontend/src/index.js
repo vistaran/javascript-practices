09a482b58c11449e21adf89babef8f7b2420f3fd
piyush281097 <piyushpatel281097@gmail.com>
2021-03-14 06:40:39 -0500

    Update index.js

--- todo/frontend/src/index.js
+++ todo/frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import $ from "jquery";
 
 ReactDOM.render(
   <React.StrictMode>
