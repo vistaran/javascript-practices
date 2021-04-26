9b00f437bdae6a9d2d797ce9d721bdedaee5457c
piyush kachhadiya <piyushpatel281097@gmail.com>
2021-04-26 15:52:24 -0500

    Create app.module.ts

---
+++ anguler Project/todo-system/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './User/login/login.component';
+import { SignupComponent } from './User/signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
